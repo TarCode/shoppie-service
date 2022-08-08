6ed865c5cb040783cd27538124800df67baeba70
tarcode <tarcode33@gmail.com>
2022-08-08 23:08:24 +0200

    Extended list model with userId

--- src/models/list.model.ts
+++ src/models/list.model.ts
@@ -4,6 +4,7 @@ import { model, Schema } from 'mongoose'
 const ListSchema = new Schema(
   {
     name: { type: String, required: [true, 'Field is required'] },
+    userId: { type: String, required: [true, 'Field is required'] },
   },
   { timestamps: true },
 )
