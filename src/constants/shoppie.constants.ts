d8c96665a1b53cc7933677ea49d189d3fdde523a
tarcode <tarcode33@gmail.com>
2022-08-09 19:55:39 +0200

    Updated port again

--- src/constants/shoppie.constants.ts
+++ src/constants/shoppie.constants.ts
@@ -2,6 +2,6 @@ import { config } from 'dotenv'
 
 config()
 
-export const PORT = 80
+export const PORT = 8080
 export const TOKEN_KEY = 'Tseppie'
 export const MONGO_URL = process.env.MONGO_URI || 'mongodb://mongo:27017/Shoppie'
