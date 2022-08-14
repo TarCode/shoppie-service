a7d2e5d503b8cf6bbca9c75d50ab70277c4dc3f7
TarCode <tarcode33@gmail.com>
2022-08-14 11:25:59 +0200

    Specs: added logger spec

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -3,9 +3,9 @@ import { Request } from 'express'
 import logger from './logger'
 
 export const logError = (req: Request, code: number, message: string) => {
-  logger.error(`${code} || ${message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
+  return logger.error(`${code} || ${message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
 }
 
 export const logSuccess = (req: Request, code: number, message: string) => {
-  logger.info(`${code} || ${message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
+  return logger.info(`${code} || ${message} - ${req.originalUrl} - ${req.method} - ${req.ip}`)
 }
---
+++ src/tests/logger.spec.ts
@@ -0,0 +1,37 @@
+import logger from '../logger/logger'
+
+jest.mock('winston', () => {
+  const mFormat = {
+    json: jest.fn(),
+    simple: jest.fn(),
+  }
+  const mTransports = {
+    Console: jest.fn(),
+    File: jest.fn(),
+  }
+  const mLogger = {
+    info: jest.fn(),
+    add: jest.fn(),
+  }
+  return {
+    format: mFormat,
+    level: 'info',
+    transports: mTransports,
+    createLogger: jest.fn(() => mLogger),
+  }
+})
+const { createLogger, format, transports } = require('winston')
+
+describe('logger', () => {
+  afterEach(() => {
+    jest.resetAllMocks()
+  })
+  it('should pass', () => {
+    logger.info('Hello world')
+
+    expect(transports.Console).toBeCalledTimes(1)
+    expect(transports.File).toBeCalledWith({ filename: 'error.log', level: 'error' })
+    expect(transports.File).toBeCalledWith({ filename: 'combined.log' })
+    expect(createLogger).toBeCalledTimes(1)
+  })
+})
